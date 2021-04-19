4b20bc55729c00eb9b88581a21d235069c3c8c5c
François Chabot <fchabot@skynet.be>
2021-04-19 15:58:15 +0200

    Bump up Be.Stateless.BizTalk.Abstractions
    refresh .build submodule
    C# 9 Rewrites
    Bump up BizTalk.Server.2020.* NuGets

--- src/Be.Stateless.BizTalk.Xml/Runtime/Caching/XsltCache.cs
+++ src/Be.Stateless.BizTalk.Xml/Runtime/Caching/XsltCache.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License
 
-// Copyright © 2012 - 2020 François Chabot
+// Copyright © 2012 - 2021 François Chabot
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ namespace Be.Stateless.BizTalk.Runtime.Caching
 		/// <summary>
 		/// Singleton <see cref="XsltCache"/> instance.
 		/// </summary>
-		public static XsltCache Instance { get; } = new XsltCache();
+		public static XsltCache Instance { get; } = new();
 
 		/// <summary>
 		/// Create the singleton <see cref="XsltCache"/> instance.
--- src/Be.Stateless.BizTalk.Xml/Xml/Xsl/XslCompiledTransformDescriptorBuilder.cs
+++ src/Be.Stateless.BizTalk.Xml/Xml/Xsl/XslCompiledTransformDescriptorBuilder.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License
 
-// Copyright © 2012 - 2020 François Chabot
+// Copyright © 2012 - 2021 François Chabot
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -80,7 +80,7 @@ namespace Be.Stateless.BizTalk.Xml.Xsl
 
 		public virtual Stateless.Xml.Xsl.XsltArgumentList BuildXsltArgumentList()
 		{
-			return new Stateless.Xml.Xsl.XsltArgumentList(_transformBase.TransformArgs);
+			return new(_transformBase.TransformArgs);
 		}
 
 		private XPathNavigator BuildNavigator()
