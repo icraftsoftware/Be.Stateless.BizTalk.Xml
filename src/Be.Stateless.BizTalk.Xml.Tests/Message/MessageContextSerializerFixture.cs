460856d81eecf918596d3bfdf98ea532734aead8
François Chabot <francois.chabot@stateless.be>
2021-10-29 16:00:53 +0200

    MessageContextSerializer
    Bump up NuGets

---
+++ src/Be.Stateless.BizTalk.Xml.Tests/Message/MessageContextSerializerFixture.cs
@@ -0,0 +1,63 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2021 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using Be.Stateless.BizTalk.ContextProperties;
+using FluentAssertions;
+using Xunit;
+
+namespace Be.Stateless.BizTalk.Message
+{
+	public class MessageContextSerializerFixture
+	{
+		[Theory]
+		[InlineData("Authorization: Bearer b3@r3r")]
+		[InlineData("Content-Type: application/xml\nAuthorization: Bearer b3@r3r\nAccept: application/xml")]
+		[InlineData("Content-Type: application/xml\rAuthorization: Bearer b3@r3r\rAccept: application/xml")]
+		[InlineData("Content-Type: application/xml\r\nAuthorization: Bearer b3@r3r\r\nAccept: application/xml")]
+		public void Serialize(string httpHeaders)
+		{
+			var redactedHttpHeaders = string.Join(
+				Environment.NewLine,
+				httpHeaders.Replace("Authorization: Bearer b3@r3r", string.Empty).Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries));
+
+			MessageContextSerializer.Serialize(
+					serializeProperty => new[] {
+						serializeProperty(BtsProperties.OutboundTransportLocation.Name, BtsProperties.OutboundTransportLocation.Namespace, @"file://c:\folder\ports\out", false),
+						serializeProperty(BtsProperties.OutboundTransportType.Name, BtsProperties.OutboundTransportType.Namespace, "FILE", true),
+						serializeProperty(FileProperties.Password.Name, FileProperties.Password.Namespace, "p@ssw0rd", false),
+						serializeProperty(new WCF.SharedAccessKey().Name.Name, new WCF.SharedAccessKey().Name.Namespace, "sh@r3d@cc3k3y", false),
+						serializeProperty(new WCF.IssuerSecret().Name.Name, new WCF.IssuerSecret().Name.Namespace, "s3cr3t", false),
+						serializeProperty(WcfProperties.HttpHeaders.Name, WcfProperties.HttpHeaders.Namespace, httpHeaders, false),
+						serializeProperty(
+							"/*[local-name()='order' and namespace-uri()='urn:schemas.stateless.be:biztalk']/*[local-name()='id' and namespace-uri()='urn:schemas.stateless.be:biztalk']",
+							"http://schemas.microsoft.com/BizTalk/2003/btsDistinguishedFields",
+							"123456789",
+							false)
+					})
+				.Should().Be(
+					@"<context xmlns:s0=""http://schemas.microsoft.com/BizTalk/2003/system-properties"" xmlns:s1=""http://schemas.microsoft.com/BizTalk/2006/01/Adapters/WCF-properties"" xmlns:s2=""http://schemas.microsoft.com/BizTalk/2003/btsDistinguishedFields"">"
+					+ @"<s0:p n=""OutboundTransportLocation"">file://c:\folder\ports\out</s0:p>"
+					+ @"<s0:p n=""OutboundTransportType"" promoted=""true"">FILE</s0:p>"
+					+ @"<s1:p n=""HttpHeaders"">" + redactedHttpHeaders + "</s1:p>"
+					+ @"<s2:p n=""/*[local-name()='order' and namespace-uri()='urn:schemas.stateless.be:biztalk']/*[local-name()='id' and namespace-uri()='urn:schemas.stateless.be:biztalk']"">123456789</s2:p>"
+					+ "</context>"
+				);
+		}
+	}
+}
---
+++ src/Be.Stateless.BizTalk.Xml/Message/MessageContextSerializer.cs
@@ -0,0 +1,95 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2021 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using System.Xml.Linq;
+using Be.Stateless.BizTalk.ContextProperties;
+using WCF;
+
+namespace Be.Stateless.BizTalk.Message
+{
+	public static class MessageContextSerializer
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static string Serialize(Func<Func<string, string, object, bool, XElement>, IEnumerable<XElement>> forEachProperty)
+		{
+			if (forEachProperty == null) throw new ArgumentNullException(nameof(forEachProperty));
+			// cache xmlns while constructing xml info set...
+			var nsCache = new XmlDictionary();
+			var xmlDocument = new XElement(
+				"context",
+				forEachProperty((name, ns, value, isPromoted) => SerializeProperty(nsCache, name, ns, value, isPromoted))
+			);
+			// ... and declare/alias all of them at the root element level to minimize xml string size
+			for (var i = 0; nsCache.TryLookup(i, out var xds); i++)
+			{
+				xmlDocument.Add(new XAttribute(XNamespace.Xmlns + "s" + xds.Key.ToString(CultureInfo.InvariantCulture), xds.Value));
+			}
+			return xmlDocument.ToString(SaveOptions.DisableFormatting);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static XElement SerializeProperty(XmlDictionary nsCache, string name, string ns, object value, bool isPromoted)
+		{
+			// give each property element a name of 'p' and store its actual name inside the 'n' attribute, which avoids
+			// the cost of the name.IsValidQName() check for each of them as the name could be an xpath expression in the
+			// case of a distinguished property
+			return name.IsSensitiveProperty()
+				? null
+				: new XElement(
+					(XNamespace) nsCache.Add(ns).Value + "p",
+					new XAttribute("n", name),
+					isPromoted ? new XAttribute("promoted", true) : null,
+					name.IsHttpHeaders(ns) ? value.ToString().Redact() : value);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static bool IsSensitiveProperty(this string name)
+		{
+			return name.IndexOf(nameof(Password), StringComparison.OrdinalIgnoreCase) > -1
+				|| name.Equals(nameof(SharedAccessKey), StringComparison.OrdinalIgnoreCase)
+				|| name.Equals(nameof(IssuerSecret), StringComparison.OrdinalIgnoreCase);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static bool IsHttpHeaders(this string name, string ns)
+		{
+			return name.Equals(WcfProperties.HttpHeaders.Name, StringComparison.OrdinalIgnoreCase)
+				&& ns.Equals(WcfProperties.HttpHeaders.Namespace, StringComparison.OrdinalIgnoreCase);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static string Redact(this string value)
+		{
+			return value.IndexOf(nameof(HttpRequestHeaders.Authorization), StringComparison.OrdinalIgnoreCase) < 0
+				? value
+				: string.Join(
+					Environment.NewLine,
+					value
+						.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+						.Select(h => h.Trim())
+						.Where(h => !h.StartsWith(nameof(HttpRequestHeaders.Authorization), StringComparison.OrdinalIgnoreCase)));
+		}
+	}
+}
